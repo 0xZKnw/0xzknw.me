f3c4019891e76a3eb352665ff5efde1e684990e0
0xZKnw <91630821+0xZKnw@users.noreply.github.com>
2025-02-16 19:05:35 +0100

    ajout du code de base

---
+++ index.js
@@ -0,0 +1,12 @@
+var body = /** @class */ (function () {
+    function body() {
+        //document.body.style.backgroundColor = '#ffeae0';
+        document.body.style.textAlign = 'center';
+        document.body.style.fontFamily = 'Arial, sans-serif';
+        var titre = document.createElement('h1');
+        titre.textContent = '0xZKnw';
+        document.body.appendChild(titre);
+    }
+    return body;
+}());
+var b = new body();
---
+++ index.ts
@@ -0,0 +1,12 @@
+class body {
+  constructor() {
+    //document.body.style.backgroundColor = '#ffeae0';
+    document.body.style.textAlign = 'center';
+    document.body.style.fontFamily = 'Arial, sans-serif';
+    const titre = document.createElement('h1');
+    titre.textContent = '0xZKnw';
+    document.body.appendChild(titre);
+  }
+}
+
+const b = new body();
