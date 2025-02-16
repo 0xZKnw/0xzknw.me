85c7f3b98015bcdb5368b8f0ff4b2d3fdf9d5977
0xZKnw <91630821+0xZKnw@users.noreply.github.com>
2025-02-16 19:41:19 +0100

    ajout textes

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 var body = /** @class */ (function () {
     function body() {
-        //document.body.style.backgroundColor = '#ffeae0';
+        //style
+        document.body.style.backgroundColor = '#f6ece7';
         document.body.style.textAlign = 'center';
-        document.body.style.fontFamily = 'Arial, sans-serif';
-        var titre = document.createElement('h1');
-        titre.textContent = '0xZKnw';
-        document.body.appendChild(titre);
+        document.body.style.margin = 'auto';
+        document.body.style.fontFamily = 'Ubuntu, sans-serif';
+        document.body.style.whiteSpace = 'pre-line';
+        //elements
+        var title = document.createElement('h1');
+        title.textContent = '0xZKnw';
+        var e1 = document.createElement('p');
+        e1.textContent = "Je suis étudiant à l'Université des sciences de Montpellier,\nje suis passionné par l'informatique et notament par la blockchain";
+        //appends
+        var cont = document.createElement('div');
+        cont.style.backgroundColor = '#f9f1ed';
+        cont.style.width = '90%';
+        cont.style.paddingTop = '1%';
+        cont.style.paddingBottom = '20px';
+        cont.style.margin = 'auto';
+        cont.style.borderRadius = '5px';
+        cont.appendChild(title);
+        cont.appendChild(e1);
+        document.body.appendChild(cont);
     }
     return body;
 }());
--- index.ts
+++ index.ts
@@ -1,11 +1,33 @@
 class body {
   constructor() {
-    //document.body.style.backgroundColor = '#ffeae0';
+    //style
+    document.body.style.backgroundColor = '#f6ece7';
     document.body.style.textAlign = 'center';
-    document.body.style.fontFamily = 'Arial, sans-serif';
-    const titre = document.createElement('h1');
-    titre.textContent = '0xZKnw';
-    document.body.appendChild(titre);
+    document.body.style.margin = 'auto';
+    document.body.style.fontFamily = 'Ubuntu, sans-serif';
+    document.body.style.whiteSpace = 'pre-line';
+
+    //elements
+    const title = document.createElement('h1');
+    title.textContent = '0xZKnw';
+
+    const e1 = document.createElement('p');
+    e1.textContent = "Je suis étudiant à l'Université des sciences de Montpellier,\nje suis passionné par l'informatique et notament par la blockchain";
+
+    //appends
+    const cont = document.createElement('div');
+
+    cont.style.backgroundColor = '#f9f1ed';
+    cont.style.width = '90%';
+    cont.style.paddingTop = '1%';
+    cont.style.paddingBottom = '20px';
+    cont.style.margin = 'auto';
+    cont.style.borderRadius = '5px';
+    
+
+    cont.appendChild(title);
+    cont.appendChild(e1);
+    document.body.appendChild(cont);
   }
 }
 
